c354becae751f75876c3c9f2fcffaf2784a50aef
Raghav Khator <rk9md@virgina.edu>
2020-10-03 14:50:58 -0400

    Created admin site and match model

--- matches/models.py
+++ matches/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+import datetime
 
-# Create your models here.
+class Match(models.Model):
+    op_name = models.CharField(max_length=200)
+    op_email = models.EmailField(max_length=200)
+    op_number = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    money_needed = models.PositiveSmallIntegerField()
+    money_spending = models.PositiveSmallIntegerField()
+    store = models.CharField(max_length=200)
+    resolved = models.BooleanField()
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
